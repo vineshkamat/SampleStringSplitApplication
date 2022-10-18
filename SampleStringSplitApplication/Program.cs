549bfbd3c246e130342d11c6236707044739b6f9
vinesh <vinesh.kamat@gmail.com>
2022-10-18 20:29:02 +0530

    Commit to main

---
+++ SampleStringSplitApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleStringSplitApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new TestSampleEntities())
+            {
+                db.Database.Log = Console.WriteLine;
+
+                var list = new[] { "nagesh", "mohan", "VAFFE" };
+
+                var result1 = db.People
+                    .Where(s => list.Contains(s.LastName))
+                    .ToList();
+
+                var joined = string.Join(",", list);
+
+                var result2 = db.People
+                    .Where(s => db.String_split(joined, ",")
+                        .Any(l => l.Value == s.LastName))
+                    .ToList();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SampleStringSplitApplication/TestSampleEntities.cs
@@ -0,0 +1,30 @@
+﻿using CodeFirstStoreFunctions;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleStringSplitApplication
+{
+    public partial class TestSampleEntities
+    {
+        public DbSet<StringSplitResult> StringSplitResults { get; set; }
+
+        [DbFunction(nameof(TestSampleEntities), nameof(String_split))]
+        [DbFunctionDetails(IsBuiltIn = true)]
+        public IQueryable<StringSplitResult> String_split(string source, string separator)
+        {
+            var sourceParameter = new ObjectParameter("Source", source);
+            var separatorParameter = new ObjectParameter("Separator", separator);
+
+            return ((IObjectContextAdapter)this).ObjectContext
+                .CreateQuery<StringSplitResult>(
+                    string.Format("[{0}].{1}", GetType().Name, "STRING_SPLIT(@Source, @Separator)"),
+                    sourceParameter, separatorParameter);
+        }
+    }
+}
